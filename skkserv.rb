9da5a2413e35b4864757e4c5e06e5c39276f2bfe
OHASHI Hideya <ohachige@gmail.com>
2009-12-13 23:46:21 +0900

    fix Social IME

--- skkserv.rb
+++ skkserv.rb
@@ -95,7 +95,13 @@ class SocialIme
         http = Net::HTTP.new('www.social-ime.com', 80)
         http.start do |h|
           res = h.get("/api/?string=#{URI.escape(q)}")
-          kanji = res.body.to_s.force_encoding('EUC-JP').split(/[\n\t]/)
+          kanji = res.body.to_s.force_encoding('EUC-JP').encode('UTF-8').split("\n")
+          kanji = kanji.map{|s|s.split("\t")}
+          s = kanji.map(&:size)
+          kanji = s.reduce(&:*).times.map do |i|
+            m = s.map{|j|(i%j).tap{i=i/j}}
+            kanji.zip(m).map{|k,l|k[l]}.join
+          end
         end
       end
       kanji
@@ -108,9 +114,9 @@ end
 if $0 == __FILE__
   dictset = [
     SKKSERVDic.new('localhost', 1178),
-    EvalDic.new,
-    WakarimasuDic.new,
-    SocialIme.new
+    #EvalDic.new,
+    #WakarimasuDic.new,
+    SocialIme.new,
   ]
   SKKServer.new.mainloop do |q|
     dictset.map{|d|d.search(q)}.select{|s|!s.nil?}.flatten
